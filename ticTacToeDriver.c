03b88b6ee054c55a3cc4bb5444fc3222b73dac9f
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-03-08 19:34:02 -0600

    Update ticTacToeDriver.c

--- ticTacToeDriver.c
+++ ticTacToeDriver.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv) {
     //o moves
     printBoard(board);
     if(gameChoice == TWO_PLAYER) {
-      userMove(board, 0);
+      userMove(board, O);
     } else if(gameChoice == ONE_PLAYER_RANDOM) {
       randomComputerMove(board);
     } else if(gameChoice == ONE_PLAYER_SMART) {
